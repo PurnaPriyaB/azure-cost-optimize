893d7e95ea05db557166d27fb439c40f94ab21cc
PurnaPriyaB <70366916+PurnaPriyaB@users.noreply.github.com>
2025-06-25 15:41:33 +0530

    Add a variables

---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "location" { default = "East US" }
+variable "resource_group_name" { default = "billing-archiver-rg" }
+variable "cosmos_account_name" { default = "billing-cosmos-db" }
+variable "function_app_name" { default = "billing-archiver-func" }
+variable "storage_account_name" { default = "billingcoldstore" }
